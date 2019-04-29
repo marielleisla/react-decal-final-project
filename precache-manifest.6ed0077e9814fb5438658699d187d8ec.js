bdcefb308f3accfdcabb4d12e6d6fefec659973f
vitali <vshypko@gmail.com>
2019-04-29 10:48:54 -0700

    Updates

---
+++ precache-manifest.6ed0077e9814fb5438658699d187d8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adb43afa883a5a4faf3e5e5aaac5608",
+    "url": "/vshypko/react-decal-final-project/index.html"
+  },
+  {
+    "revision": "f1af987a83a03a997eb9",
+    "url": "/vshypko/react-decal-final-project/static/css/main.9119c009.chunk.css"
+  },
+  {
+    "revision": "efe1380c02f8ac529339",
+    "url": "/vshypko/react-decal-final-project/static/js/2.d9b17a21.chunk.js"
+  },
+  {
+    "revision": "f1af987a83a03a997eb9",
+    "url": "/vshypko/react-decal-final-project/static/js/main.ca96e5ab.chunk.js"
+  },
+  {
+    "revision": "6c6b94c96db01e2d7943",
+    "url": "/vshypko/react-decal-final-project/static/js/runtime~main.8b4f8d2a.js"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/vshypko/react-decal-final-project/static/media/Spotify_Logo_RGB_Green.11dc437a.png"
+  }
+]);
